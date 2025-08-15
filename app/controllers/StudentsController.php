2858df8e6624e20c143dca74cc632e02421d8f35
Rhoo <@melomelodeon@gmail.com>
2025-08-16 02:50:17 +0800

    modified routes.php, StudentsController.php

--- app/controllers/StudentsController.php
+++ app/controllers/StudentsController.php
@@ -1,14 +1,40 @@
 <?php
-defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
+defined('PREVENT_DIRECT_ACCESS') or exit('No direct script access allowed');
 
 /**
  * Controller: StudentsController
  * 
  * Automatically generated via CLI.
  */
-class StudentsController extends Controller {
+class StudentsController extends Controller
+{
     public function __construct()
     {
         parent::__construct();
+    }
+
+    function selectFunction($ulrParam)
+    {
+        switch ($ulrParam) {
+            case 'create':
+                $this->call->view('StudentsViewPage');
+                break;
+        }
+    }
+
+    function get_all()
+    {
+    }
+
+    function create()
+    {
+    }
+
+    function update()
+    {
+    }
+
+    function delete()
+    {
     }
 }
