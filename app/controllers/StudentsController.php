1000383559b73a9bedbfae5c9e1a013ee6817038
Rhoo <@melomelodeon@gmail.com>
2025-08-16 02:09:22 +0800

    three new files added.StudentsController, StudentViewPage, StudentsModel

---
+++ app/controllers/StudentsController.php
@@ -0,0 +1,14 @@
+<?php
+defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
+
+/**
+ * Controller: StudentsController
+ * 
+ * Automatically generated via CLI.
+ */
+class StudentsController extends Controller {
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
