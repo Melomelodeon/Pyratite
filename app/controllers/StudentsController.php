b18f7cd8f55f527ed9380f020f0bb956e573fd9d
Rhoo <@melomelodeon@gmail.com>
2025-08-17 01:29:42 +0800

    fixed function that selecting which functions to execute. added funtionality for get_all() added funtionality for create()

--- app/controllers/StudentsController.php
+++ app/controllers/StudentsController.php
@@ -11,30 +11,40 @@ class StudentsController extends Controller
     public function __construct()
     {
         parent::__construct();
+        $this->call->model('StudentsModel');
     }
 
     function selectFunction($ulrParam)
     {
         switch ($ulrParam) {
-            case 'create':
-                $this->call->view('StudentsViewPage');
+            case 'read':
+                $this->get_all();
+                break;
+            case 'write':
+                $this->create();
                 break;
         }
     }
 
     function get_all()
     {
+        echo "<pre>";
+        var_dump($this->StudentsModel->readDb());
+        echo "</pre>";
     }
 
     function create()
     {
+        $this->StudentsModel->insertIntoDb();
     }
 
     function update()
     {
+        $this->call->view('welcome_page');
     }
 
     function delete()
     {
+        $this->call->view('welcome_page');
     }
 }
--- app/models/StudentsModel.php
+++ app/models/StudentsModel.php
@@ -1,12 +1,13 @@
 <?php
-defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
+defined('PREVENT_DIRECT_ACCESS') or exit('No direct script access allowed');
 
 /**
  * Model: StudentsModel
  * 
  * Automatically generated via CLI.
  */
-class StudentsModel extends Model {
+class StudentsModel extends Model
+{
     protected $table = '';
     protected $primary_key = 'id';
 
@@ -14,4 +15,19 @@ class StudentsModel extends Model {
     {
         parent::__construct();
     }
+
+    function readDb()
+    {
+        return $this->db->table('lava_lust_test')->get_all();
+    }
+
+    function insertIntoDb()
+    {
+        $this->db->table('lava_lust_test')->insert([
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'email' => 'john@example.com'
+        ]);
+    }
+
 }
