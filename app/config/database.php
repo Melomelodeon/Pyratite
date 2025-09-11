5297f7ec64a754f7847ebe72ac97c19563748560
Rhoo <melomelodeon@gmail.com>
2025-09-11 11:10:07 +0800

    updated database credentials

--- app/config/database.php
+++ app/config/database.php
@@ -58,16 +58,16 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 */
 
 $database['main'] = array(
-    'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
-    'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'lava_lust_db_test',
-    'charset'	=> 'utf8mb4',
-    'dbprefix'	=> '',
-    // Optional for SQLite
+    'driver'    => 'mysql',
+    'hostname'  => 'sql12.freesqldatabase.com',
+    'port'      => '3306',
+    'username'  => 'sql12798159',
+    'password'  => 'TQiald1AMa',
+    'database'  => 'sql12798159',
+    'charset'   => 'utf8mb4',
+    'dbprefix'  => '',
     'path'      => ''
 );
+
 
 ?>
